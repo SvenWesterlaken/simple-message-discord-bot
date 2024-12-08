f50913fe5a4284e8dec93fabda4873686e3a7308
Sven <sven.westerlaken@gmail.com>
2024-12-09 00:58:54 +0100

    add typing construction

--- example_bot.py
+++ example_bot.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+import asyncio
 
 load_dotenv()
 
@@ -15,12 +16,23 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+    print(message.channel)
+
     if message.author == client.user or message.author.bot:
         # We don't answer to bots or if we are logged in as the bot
         return
 
+    createdThread = await message.create_thread(
+        name=message.content, 
+        reason='Question: ' + message.content
+    )
+
+    async with createdThread.typing():
+        # simulate something heavy
+        await asyncio.sleep(10)
+
     # Send back the legnth of the message together with the users name
-    await message.channel.send('Hi, ' + message.author.name + '! Your message was ' + str(len(message.content)) + ' characters long.')
+    await createdThread.send('Hi, ' + message.author.name + '! Your message was ' + str(len(message.content)) + ' characters long.')
 
 # Run the client with the API key from the environment set
 client.run(os.getenv('DISCORD_API_KEY'));
