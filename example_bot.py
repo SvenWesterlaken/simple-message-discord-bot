13460b68d592163b1ef9be891b93dd8100d86ce3
Sven <sven.westerlaken@gmail.com>
2024-12-14 18:55:39 +0100

    replace basic logic of listening for messages with button logic

--- example_bot.py
+++ example_bot.py
@@ -1,6 +1,8 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
+from discord.ui import Button
 import asyncio
 
 load_dotenv()
@@ -8,31 +10,52 @@ load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='!mr ', intents=intents)
+
+class QuestionSetupView(discord.ui.View):
+    
+    @discord.ui.button(label='Ask a Question', style=discord.ButtonStyle.blurple)
+    async def ask_question(self, interaction: discord.Interaction, button: discord.ui.Button):
+        print(interaction)
+        await interaction.response.send_message('You asked a question!')
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    print(f'We have logged in as "{client.user}"')
+    # await discordClient.tree.sync()
 
-@client.event
-async def on_message(message):
-    print(message.channel)
+@client.command()
+@commands.has_permissions(administrator=True)
+async def initialize(ctx):
+    await ctx.message.delete()
+    await ctx.channel.purge()
 
-    if message.author == client.user or message.author.bot:
-        # We don't answer to bots or if we are logged in as the bot
-        return
-
-    createdThread = await message.create_thread(
-        name=message.content, 
-        reason='Question: ' + message.content
+    await ctx.send(
+        view = QuestionSetupView()
     )
 
-    async with createdThread.typing():
-        # simulate something heavy
-        await asyncio.sleep(10)
+# @client.event
+# async def on_message(message):
+#     print(message)
 
-    # Send back the legnth of the message together with the users name
-    await createdThread.send('Hi, ' + message.author.name + '! Your message was ' + str(len(message.content)) + ' characters long.')
+#     if message.author == client.user or message.author.bot:
+#         # We don't answer to bots or if we are logged in as the bot
+#         return
+
+#     if message.channel.type == 'public_thread' or 'private_thread':
+#         thread = message.channel
+#     else:
+#         thread = await message.create_thread(
+#             name=message.content, 
+#             reason='Question: ' + message.content
+#         )
+
+#     async with thread.typing():
+#         # simulate something heavy
+#         await asyncio.sleep(10)
+
+#     # Send back the legnth of the message together with the users name
+#     await thread.send('Hi, ' + message.author.name + '! Your message was ' + str(len(message.content)) + ' characters long.')
 
 # Run the client with the API key from the environment set
 client.run(os.getenv('DISCORD_API_KEY'));
